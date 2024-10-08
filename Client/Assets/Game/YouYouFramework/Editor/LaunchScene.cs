39344a78deff0507f8b262397de9e62eec2711aa
abba01001 <1324058099@qq.com>
2024-10-08 16:25:42 +0800

    更新

--- Client/Assets/Game/YouYouFramework/Editor/LaunchScene.cs
+++ Client/Assets/Game/YouYouFramework/Editor/LaunchScene.cs
@@ -15,39 +15,28 @@ public class LanuchScene
     [MenuItem("启动场景/游戏")]
     static void GameScene()
     {
+        Menu.SetChecked("启动场景/关卡编辑器", false);
         Menu.SetChecked("启动场景/游戏", true);
         Menu.SetChecked("启动场景/手动选择", false);
         SceneAsset scene = AssetDatabase.LoadAssetAtPath<SceneAsset>("Assets/Game/Scene_Launch.unity");
         EditorSceneManager.playModeStartScene = scene;
     }
 
-    // [MenuItem("启动场景/AI模拟器")]
-    // static void AIScene()
-    // {
-    //     Menu.SetChecked("启动场景/游戏", false);
-    //     Menu.SetChecked("启动场景/AI模拟器", true);
-    //     Menu.SetChecked("启动场景/关卡编辑器", false);
-    //     Menu.SetChecked("启动场景/手动选择", false);
-    //     SceneAsset scene = AssetDatabase.LoadAssetAtPath<SceneAsset>("Assets/Scenes/AIScene.unity");
-    //     EditorSceneManager.playModeStartScene = scene;
-    // }
-    //
-    // [MenuItem("启动场景/关卡编辑器")]
-    // static void LevelScene()
-    // {
-    //     Menu.SetChecked("启动场景/游戏", false);
-    //     Menu.SetChecked("启动场景/AI模拟器", false);
-    //     Menu.SetChecked("启动场景/关卡编辑器", true);
-    //     Menu.SetChecked("启动场景/手动选择", false);
-    //     SceneAsset scene = AssetDatabase.LoadAssetAtPath<SceneAsset>("Assets/Scenes/EditorScene.unity");
-    //     EditorSceneManager.playModeStartScene = scene;
-    // }
+    [MenuItem("启动场景/关卡编辑器")]
+    static void MapEditorScene()
+    {
+        Menu.SetChecked("启动场景/关卡编辑器", true);
+        Menu.SetChecked("启动场景/游戏", false);
+        Menu.SetChecked("启动场景/手动选择", false);
+        SceneAsset scene = AssetDatabase.LoadAssetAtPath<SceneAsset>("Assets/Game/MapEditor.unity");
+        EditorSceneManager.playModeStartScene = scene;
+    }
 
     [MenuItem("启动场景/手动选择")]
     static void AnyScene()
     {
         Menu.SetChecked("启动场景/游戏", false);
-        // Menu.SetChecked("启动场景/AI模拟器", false);
+        Menu.SetChecked("启动场景/关卡编辑器", false);
         // Menu.SetChecked("启动场景/关卡编辑器", false);
         Menu.SetChecked("启动场景/手动选择", true);
         EditorSceneManager.playModeStartScene = null;
