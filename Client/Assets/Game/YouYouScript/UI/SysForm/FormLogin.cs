3f8605eb29627aa7d50cf1fa2d51374a4f1cc75f
abba01001 <1324058099@qq.com>
2025-10-28 16:32:52 +0800

    Nuget安装

--- Client/Assets/Game/YouYouScript/UI/SysForm/FormLogin.cs
+++ Client/Assets/Game/YouYouScript/UI/SysForm/FormLogin.cs
@@ -46,8 +46,8 @@ public class FormLogin : UIFormBase
         //loginBtn.GetComponent<Image>().SetImage("Assets/Game/Download/Atlas/Textures/Common","JoyBg.png",true);
         
          await GameEntry.Net.ConnectServerAsync();
-         // GameEntry.Net.Requset.c2s_request_register("a123456789","99999");
-         GameEntry.Net.Requset.c2s_request_login("a123456789","99999");
+         // GameEntry.Net.Requset.c2s_request_register("a888","99999");
+         GameEntry.Net.Requset.c2s_request_login("a888","99999");
         return;
         
 //将 Base64 字符串转换为二进制数据
